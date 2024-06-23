a00f0e69eee78e8938081829fd899684b53f8e8f
Jon C <me@jonc.dev>
2024-06-23 13:27:41 +0200

    [SOL] Prohibit printing in SBF backend (#2)
    #### Problem
    
    It isn't possible to use helpers like `std.testing.expectEqual` in SBF
    because the backend allows printing, and fails compilation.
    
    #### Solution
    
    Specify that the solana backend doesn't allow printing.

--- zig/lib/std/testing.zig
+++ zig/lib/std/testing.zig
@@ -32,7 +32,7 @@ pub var allocator_instance: std.heap.GeneralPurposeAllocator(.{
 pub var log_level = std.log.Level.warn;
 
 // Disable printing in tests for simple backends.
-pub const backend_can_print = !(builtin.zig_backend == .stage2_spirv64 or builtin.zig_backend == .stage2_riscv64);
+pub const backend_can_print = !(builtin.zig_backend == .stage2_spirv64 or builtin.zig_backend == .stage2_riscv64 or builtin.os.tag == .solana);
 
 fn print(comptime fmt: []const u8, args: anytype) void {
     if (@inComptime()) {
